8ccd9a8f83dc892851a4710e118013db636449f2
Anata Arisa <kakata28419@gmail.com>
2023-10-01 22:47:08 +0700

    Init Project

---
+++ BusinessObjects/Cartoon.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BusinessObjects
+{
+    public class Cartoon
+    {
+
+    }
+}
---
+++ WindowsFormsDemo/TruyenTranh.cs
@@ -0,0 +1,23 @@
+﻿namespace WindowsFormsDemo
+{
+    public class TruyenTranh
+    {
+        public int MaTruyen { get; set; }
+        public string NamXuatBan { get; set; }
+        public string TacGia { get; set; }
+        public string TenTruyen { get; set; }
+        public string NhaXuatBan { get; set; }
+        public int SoLuong { get; set; }
+
+        public TruyenTranh(int matruyen, string nxb, string tacgia, string tentruyen, string nhaxb,
+            int soluong)
+        {
+            this.MaTruyen = matruyen;
+            this.NamXuatBan = nxb;
+            this.TacGia = tacgia;
+            this.TenTruyen = tentruyen;
+            this.NhaXuatBan = nhaxb;
+            this.SoLuong = soluong;
+        }
+    }
+}

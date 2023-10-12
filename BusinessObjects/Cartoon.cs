b7b8eb980f36127de27f2642c153556804c408dd
Anata Arisa <kakata28419@gmail.com>
2023-10-12 17:03:02 +0700

    vinh/add business object

--- BusinessObjects/Cartoon.cs
+++ BusinessObjects/Cartoon.cs
@@ -1,9 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BusinessObjects
 {
-    public class Cartoon
+    public partial class Cartoon
     {
+        public decimal CartoonId { get; set; }
+        public string CartoonName { get; set; }
+        public DateTime LaunchDate { get; set; }
+        public int CartoonType { get; set; }
+        public string ShortDescription { get; set; }
+        public string Actors { get; set; }
+        public string Director { get; set; }
+        public string Country { get; set; }
 
+        public virtual CartoonType CartoonTypeNavigation { get; set; }
     }
 }
---
+++ BusinessObjects/CartoonType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects
+{
+    public partial class CartoonType
+    {
+        public CartoonType()
+        {
+            Cartoons = new HashSet<Cartoon>();
+        }
+
+        public int CartoonTypeId { get; set; }
+        public string CartoonName { get; set; }
+
+        public virtual ICollection<Cartoon> Cartoons { get; set; }
+    }
+}

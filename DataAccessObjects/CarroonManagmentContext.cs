f085b177381178f9168a1710fed983c654a0fea0
Anata Arisa <kakata28419@gmail.com>
2023-10-12 17:05:45 +0700

    vinh/add business object

---
+++ DataAccessObjects/CarroonManagmentContext.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using BusinessObjects;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessObjects
+{
+    public partial class CarroonManagmentContext : DbContext
+    {
+        public CarroonManagmentContext()
+        {
+        }
+
+        public CarroonManagmentContext(DbContextOptions<CarroonManagmentContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cartoon> Cartoons { get; set; }
+        public virtual DbSet<CartoonType> CartoonTypes { get; set; }
+
+        private string GetConnectionString()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", true, true).Build();
+            return configuration["ConnectionStrings:DefaultConnectionString"];
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+/*#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.*/
+                optionsBuilder.UseSqlServer(GetConnectionString());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cartoon>(entity =>
+            {
+                entity.ToTable("Cartoon");
+
+                entity.Property(e => e.CartoonId)
+                    .HasColumnType("decimal(20, 0)")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("cartoonID");
+
+                entity.Property(e => e.Actors)
+                    .IsRequired()
+                    .HasMaxLength(120)
+                    .HasColumnName("actors");
+
+                entity.Property(e => e.CartoonName)
+                    .IsRequired()
+                    .HasMaxLength(120)
+                    .HasColumnName("cartoonName");
+
+                entity.Property(e => e.CartoonType).HasColumnName("cartoonType");
+
+                entity.Property(e => e.Country)
+                    .IsRequired()
+                    .HasMaxLength(120)
+                    .HasColumnName("country");
+
+                entity.Property(e => e.Director)
+                    .IsRequired()
+                    .HasMaxLength(120)
+                    .HasColumnName("director");
+
+                entity.Property(e => e.LaunchDate)
+                    .HasColumnType("date")
+                    .HasColumnName("launchDate");
+
+                entity.Property(e => e.ShortDescription)
+                    .IsRequired()
+                    .HasColumnType("text")
+                    .HasColumnName("shortDescription");
+
+                entity.HasOne(d => d.CartoonTypeNavigation)
+                    .WithMany(p => p.Cartoons)
+                    .HasForeignKey(d => d.CartoonType)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_CartoonType");
+            });
+
+            modelBuilder.Entity<CartoonType>(entity =>
+            {
+                entity.ToTable("CartoonType");
+
+                entity.Property(e => e.CartoonTypeId).HasColumnName("cartoonTypeID");
+
+                entity.Property(e => e.CartoonName)
+                    .HasMaxLength(120)
+                    .HasColumnName("cartoonName");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DataAccessObjects/CourseTypeDAO.cs
@@ -0,0 +1,28 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessObjects
+{
+    public class CourseTypeDAO
+    {
+        public static List<CartoonType> GetCartoonTypes()
+        {
+            List<CartoonType> listCartoonType = new List<CartoonType>();
+            try
+            {
+                using (var context = new CarroonManagmentContext())
+                {
+                    listCartoonType = context.CartoonTypes.ToList();
+                }
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return listCartoonType; 
+        }
+    }
+}
